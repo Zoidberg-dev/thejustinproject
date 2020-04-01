93a4648d1ce2b3c646afbe3ca377db1ec4cbee95
WingzOfDeath <wingzofdeath317@gmail.com>
2020-04-01 21:03:35 +0200

    12

--- my-app/src/routes/index.svelte
+++ my-app/src/routes/index.svelte
@@ -40,7 +40,7 @@
 
 <figure>
 	<img alt='Borat' src='great-success.png'>
-	<figcaption>HiGH FIVE!</figcaption>
+	<figcaption>Cyka</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>

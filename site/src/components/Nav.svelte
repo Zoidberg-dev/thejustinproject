a1a89ae5849a543dc2c56aeb1d36ebfee0d0faa2
bram.buunk <bram.buunk@topicus.nl>
2020-04-04 22:42:12 +0200

    test

---
+++ site/src/components/Nav.svelte
@@ -0,0 +1,55 @@
+<section>
+
+  <div>
+    <TabBar tabs={iconTabs} let:tab>
+      	<Tab {tab} on:click={()=> navigate(tab)}>
+        <Icon class="material-icons">{tab.icon}</Icon>
+        <Label>{tab.label}</Label>
+      </Tab>
+    </TabBar>
+  </div>
+</section>
+
+<script>
+  import { goto } from '@sapper/app';
+  import Tab, {Icon, Label} from '@smui/tab';
+  import TabBar from '@smui/tab-bar';
+  import Button from '@smui/button';
+  import {stores} from '@sapper/app';
+  let active = 'Home';
+  const {page} = stores();
+
+  let iconTabs = [
+    {
+      icon: 'apps',
+      label: 'Home',
+      route: "/"
+    },
+    {
+      icon: 'accessible_forward',
+      label: 'Justin',
+      route: "/justin"
+    },
+    {
+      icon: 'smoking_rooms',
+      label: 'Bram',
+      route: "/bram"
+    },
+    {
+      icon: 'assignment',
+      label: 'To do',
+      route: "/todo"
+    }
+  ];
+
+function navigate(tab){ 
+  goto(tab.route);
+  }
+
+</script>
+
+<style>
+  section > div {
+    margin-bottom: 40px;
+  }
+</style>

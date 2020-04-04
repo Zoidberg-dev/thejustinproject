88fe54aba58338bb98d79a205bfa15f13b607232
bram.buunk <bram.buunk@topicus.nl>
2020-04-04 22:49:20 +0200

    reorder map

--- site/src/routes/bram.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-  <title>Bram</title>
-</svelte:head>
-
-<section>
-    <div><p>Bram</p></div>
-</section>
--- site/src/routes/index.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-  <title>The Justin Project</title>
-</svelte:head>
-
-<section>
-  <div><p>Home</p></div>
-</section>
